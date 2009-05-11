526044689c73ad222818954b99a0d764db1188c1
maxvujovic <maxvujovic@88aed6c4-299c-11de-a747-197756c87c9e>
2009-05-11 02:47:41 +0000

    almost done with page_customer_list.php, and generic listing module

---
+++ src/x/obj_search_bar.php
@@ -0,0 +1,84 @@
+<?
+
+class SearchBar
+{
+	public static function display($obj_name, $search_box_value, $order_by_options, $order_by_value, $asc_or_desc_value, $cur_page_num)
+	{
+		// onclick="document.location=\'page_' . $obj_name . '_list.php?f_search=\' + escape(form_search.f_search.value) + \'&amp;f_order_by=\' + escape(form_search.f_order_by.value) + \'&amp;f_asc_or_desc=\' + escape(form_search.f_asc_or_desc.value)"
+	
+		//draw search form
+		echo('
+		<form name="form_search" method="post" action="page_' . $obj_name . '_list.php">
+			<table width="100%" cellspacing="0" cellpadding="0">
+				<tr>
+					<td width="33%" align="left" valign="middle">			
+						<input name="f_page" type="hidden" value="' . $cur_page_num . '"/>
+						<input name="f_search" type="text" width="30" class="textbox" value="' . IO::prepout_sl($search_box_value, false) . '"/>
+						<input name="f_search_btn" class="button" type="submit" value="Search"/>
+					</td>
+					<td align="left" valign="middle" width="33%">
+		');
+		
+		//draw order by select box
+		self::echo_order_by_select_box($order_by_options, $order_by_value);
+		
+		echo('
+					</td>
+					<td  width="33%" align="left" valign="middle">
+		');
+		
+		//draw ascending sort or descending sort radio buttons
+		self::echo_asc_or_desc_btns($asc_or_desc_value);
+		
+		echo('
+					</td></tr>
+				</table>		
+			</form>
+		');
+	}
+	
+	private function echo_order_by_select_box($order_by_options, $order_by_value)
+	{
+		echo('
+			Order&nbsp;By:&nbsp;<select name="f_order_by" class="textbox" onchange="form_search.f_page.value=1; form_search.submit();">
+		');
+		
+		$cnt = count($order_by_options);
+		for($i = 0; $i < $cnt; $i++)
+		{
+			echo('<option value="' . $order_by_options[$i][1] . '" ' . (($order_by_value == $order_by_options[$i][1]) ? 'selected' : '') . '>' . $order_by_options[$i][0] . '</option>');
+		}
+		
+		echo('
+			</select>		
+		');
+	}
+
+	private function echo_asc_or_desc_btns($asc_or_desc_value)
+	{
+		echo('
+			<table cellspacing="0" cellpadding="0">
+				<tr>
+					<td align="left" valign="middle">			
+						Ascending:
+					</td>
+					<td align="left" valign="middle">			
+						<input type="radio" name="f_asc_or_desc" value="asc" ' . (($asc_or_desc_value == 'asc') ? 'checked' : '') . ' onchange="form_search.f_page.value=1; form_search.submit();">
+					</td>
+					<td align="left" valign="middle">			
+						&nbsp;&nbsp;&nbsp;
+					</td>
+					<td align="left" valign="middle">			
+						Descending:
+					</td>
+					<td align="left" valign="middle">			
+						<input type="radio" name="f_asc_or_desc" value="desc" ' . (($asc_or_desc_value == 'desc') ? 'checked' : '') . ' onchange="form_search.f_page.value=1; form_search.submit();">
+					</td>
+				</tr>
+			</table>
+		');
+	}
+}
+
+
+?>
---
+++ src/x/page_generic_list.php
@@ -0,0 +1,163 @@
+<?php
+
+require_once('obj_search_bar.php');
+require_once('obj_page_num_nav.php');
+
+class PageGenericList
+{
+	//*** CONSTANTS ***
+	private static $RESULTS_PER_PAGE = 2;
+	private static $MAX_PAGES_IN_NAV_BAR = 10;
+	
+	private static $DEFAULT_ORDER_BY_OPTION_INX = 0;
+	private static $DEFAULT_ASC_OR_DESC = 'desc';
+	
+	//*** CONFIGURATION/CONSTRUCTION MEMBERS ***
+	private $m_tab_inx;
+	private $m_obj_name;
+	private $m_order_by_options;
+	private $m_list_object;
+
+	
+	
+	//*** MEMBERS ***
+	private $m_rows;
+	private $m_num_results;
+	
+	private $f_page;
+	private $f_mode;
+	private $f_search;
+	private $f_order_by;
+	private $f_asc_or_desc;
+	
+	//*** FUNCTIONS ***
+	public function __construct($the_tab_inx, $the_obj_name, $the_order_by_options, $the_list_object)
+	{
+		$this->m_tab_inx = $the_tab_inx;
+		$this->m_obj_name = $the_obj_name;
+		$this->m_order_by_options = $the_order_by_options;
+		$this->m_list_object = $the_list_object;
+	}
+	
+	//execution entry point
+	public function run()
+	{
+		session_start();
+		DB::connect();
+	
+		LoginManager::assert_auth_level(LoginManager::$AUTH_READ_ONLY);	//CHANGE required authorization level for this page, ADMIN is the strictest
+	
+		$this->get_input(); 
+		
+		$this->verify_input();
+		
+		$this->process_input();
+		
+		$this->show_output();
+	}
+	
+	private function get_input()
+	{
+		$this->f_page = IO::get_input_sl_pg('f_page', 'integer', 1);
+		$this->f_mode = IO::get_input_sl_pg('f_mode', 'string');
+		$this->f_search = IO::get_input_sl_pg('f_search', 'string');
+		$this->f_order_by = IO::get_input_sl_pg('f_order_by', 'string', $this->m_order_by_options[self::$DEFAULT_ORDER_BY_OPTION_INX][1]);
+		$this->f_asc_or_desc = IO::get_input_sl_pg('f_asc_or_desc', 'string', self::$DEFAULT_ASC_OR_DESC);
+	}
+	
+	private function verify_input()
+	{
+		//verify/force that order by is one of the predefined values (not an sql injection), this is necessary because it is a column name and not a value (so is not in quotes) 
+		$inputted_order_by = $this->f_order_by;
+		$this->f_order_by = $this->m_order_by_options[self::$DEFAULT_ORDER_BY_OPTION_INX][1];
+		foreach($this->m_order_by_options as $cur_order_by_option)
+		{
+			if($inputted_order_by == $cur_order_by_option[1])
+			{
+				$this->f_order_by = $cur_order_by_option[1];
+				break;
+			}
+		}
+		
+		//verify/force the asc/desc is the asc or desc keyword, not a command like "drop table;"
+		$inputted_asc_or_desc = strtolower($this->f_asc_or_desc);
+		if($inputted_asc_or_desc != 'asc' && $inputted_asc_or_desc != 'desc') $this->f_asc_or_desc = self::$DEFAULT_ASC_OR_DESC;
+	}
+	
+	private function process_input()
+	{
+		//TODO: trash sort
+		$offset = ($this->f_page-1)*self::$RESULTS_PER_PAGE;
+		$limit = self::$RESULTS_PER_PAGE;
+		
+		//special query for sort by trash_flag, ignores asc or desc
+		if($this->f_order_by == 'trash_flag') $order_by_clause = 'ORDER BY trash_flag DESC, relevance DESC ';
+		else $order_by_clause = 'ORDER BY trash_flag ASC, ' . $this->f_order_by . ' ' . $this->f_asc_or_desc . ' ';
+		
+		echo $order_by_clause;
+		
+		//if empty search text, get and list everything 
+		if($this->f_search == '')
+		{
+			$this->m_rows = DB::get_all_rows_fq('
+				SELECT SQL_CALC_FOUND_ROWS ' . $this->m_list_object->get_needed_fields() . ', 
+				1 AS relevance
+				FROM ' . $this->m_obj_name . 's ' .
+				$order_by_clause .
+				'LIMIT ' . $offset . ',' . $limit
+			);			
+		}
+		//nonempty search text
+		else
+		{
+			$encoded_search = DB::encode_small_words_search($this->f_search);
+			$this->m_rows = DB::get_all_rows_fq('
+				SELECT SQL_CALC_FOUND_ROWS ' . $this->m_list_object->get_needed_fields() . ', 
+				MATCH(search_words) AGAINST(\'' . $encoded_search . '\' IN BOOLEAN MODE) as relevance
+				FROM ' . $this->m_obj_name . 's WHERE MATCH(search_words) AGAINST(\'' . $encoded_search . '\' IN BOOLEAN MODE) ' .
+				$order_by_clause . ' 
+				LIMIT ' . $offset . ',' . $limit
+			);
+		}
+			
+		$this->m_num_results = DB::get_num_rows_found();    //total rows found matching the where clause, ignoring the limit clause
+	}
+	
+	private function show_output($err_msg = '')
+	{
+		//echo the outer area with the correct tab highlighted for this page
+		ObjOuterArea::echo_top($this->m_tab_inx);
+		
+		//echo inner area html here
+		/*
+		//Error Printing Example:
+		if($err_msg != '') echo('<font class="text_error">' . $err_msg . '</font>');
+		*/
+		
+		//page title
+		echo('<div align="left" class="text_title">Search ' . ucfirst($this->m_obj_name) . 's</div>');
+
+		//draw search bar
+		//prototype: SearchBar::display($obj_name, $search_box_value, $order_by_options, $order_by_value, $asc_or_desc_value
+		SearchBar::display($this->m_obj_name, $this->f_search, $this->m_order_by_options, $this->f_order_by, $this->f_asc_or_desc, $this->f_page);
+		
+		//display the top page number navigation bar
+		//public function echo_top_bar($bar_width, $ws_border_top, $ws_border_bottom, $page_num, $num_results, $results_per_page, $action_script_left, $action_script_right, $max_pages=5, $left_col='', $last_page=-1)
+		$page_nav_bar = new ObjPageNavBar();
+		$page_nav_bar->echo_top_bar('80%', 0, 10, $this->f_page, $this->m_num_results, self::$RESULTS_PER_PAGE, 'form_search.f_page.value=', '; form_search.submit();', self::$MAX_PAGES_IN_NAV_BAR);		
+		
+		//draw results
+		//prototype: display($action_box_mode, $cust_info_arr, $num_total_results, $cur_page_num, $page_name)
+		$this->m_list_object->display($this->f_mode, $this->m_rows);
+		
+		//display the bottom page number navigation bar
+		$page_nav_bar->echo_bottom_bar(0, 0);		
+		
+		ObjOuterArea::echo_bottom();
+	
+		//output is always the last thing done when called
+		exit();
+	}
+}
+
+?>
