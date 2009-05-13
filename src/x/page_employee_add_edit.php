0360aae6e85394ec6554209fe4827037b5bdf39d
abonomo <abonomo@88aed6c4-299c-11de-a747-197756c87c9e>
2009-05-13 04:12:55 +0000

    accidentally submitted wrong version in previous revision

--- src/x/page_employee_add_edit.php
+++ src/x/page_employee_add_edit.php
@@ -3,9 +3,9 @@
 /*
  * General TODO's for this page:
  * TODO: implement a (good) method for admin/users to modify passwords
- * TODO: should employees be able to modify their office location?
- * TODO: Change GUI according to if it's admin or a regular employee
- * TODO: verify input function...
+ * TODO: make it so auth_level is changed appropriately when admin is editing employee
+ * TODO: better way to change modifiable fields for non-admin users?
+ * TODO: check verify input function...anything else to implement here?
  */
 
 require_once('framework.php');
@@ -34,12 +34,13 @@ class PageEmployeeAddEdit
 	private $f_office_phone_number;
 	private $f_cell_phone_number;
 	private $f_fax_number;
+	private $f_title;
 	
 	//*** FUNCTIONS ***
 	//execution entry point
 	private function isAdmin()
 	{
-		return LoginManager::meets_auth_level((LoginManager::$AUTH_ADMIN));
+		return LoginManager::meets_auth_level(LoginManager::$AUTH_ADMIN);
 	}
 	
 	public function run()
@@ -50,12 +51,12 @@ class PageEmployeeAddEdit
 		LoginManager::assert_auth_level(LoginManager::$AUTH_LOGIN);	
 		
 		//TODO: is this secure?
-		if(!$this->isAdmin())
+		if(!($this->isAdmin()))
 		{
 			$this->f_mode = 'edit';
 			$this->f_id = LoginManager::get_id();
 		}
-			
+		
 		$this->get_input(); 
 		
 		$this->verify_input();
@@ -69,7 +70,7 @@ class PageEmployeeAddEdit
 	{
 		//get mode if admin, otherwise mode is set to 'edit'
 		if($this->isAdmin())
-			$this->f_mode = IO::get_input_sl_g('f_mode','string');
+			$this->f_mode = IO::get_input_sl_g('f_mode','string');	
 				
 		$this->f_action = IO::get_input_sl_g('f_action','string');
 		
@@ -90,6 +91,7 @@ class PageEmployeeAddEdit
 				$this->f_dept_name	= IO::get_input_sl_pg('f_dept_name','string');
 				$this->f_icode 		= IO::get_input_sl_pg('f_icode', 'string');
 				$this->f_id 		= IO::get_input_sl_pg('f_id', 'integer');
+				$this->f_title		= IO::get_input_sl_pg('f_title', 'string');
 			}
 			
 			$this->f_cell_phone_number 		= IO::get_input_sl_pg('f_cell_phone_number','string');
@@ -114,17 +116,31 @@ class PageEmployeeAddEdit
 			}
 						
 			//get values from database
-			$employee_info = DB::get_single_row_fq
-			(
-				'SELECT icode, email, password, auth_level, first_name, last_name
-				FROM employees WHERE id=\'' . $this->f_id . '\''
-			);
+			//TODO: password stuff
+			if($this->isAdmin())
+			{
+				$employee_info = DB::get_single_row_fq
+				(
+					'SELECT icode, email, password, auth_level, first_name, last_name, dept_name, office_location, office_phone_number, cell_phone_number, fax_number, title
+					FROM employees WHERE id=\'' . $this->f_id . '\''
+				);
+			}
+			else
+			{
+				$employee_info = DB::get_single_row_fq
+				(
+					'SELECT email, password, first_name, last_name, office_location, office_phone_number, cell_phone_number, fax_number
+					FROM employees WHERE id=\'' . $this->f_id . '\''
+				);
+			}
+			
 			
 			if($this->isAdmin())
 			{
 				$this->f_auth_level	= $employee_info['auth_level'];
 				$this->f_dept_name	= $employee_info['dept_name'];
 				$this->f_icode		= $employee_info['icode'];
+				$this->f_title		= $employee_info['title'];
 			}
 			
 			$this->f_cell_phone_number 		= $employee_info['cell_phone_number'];
@@ -141,7 +157,29 @@ class PageEmployeeAddEdit
 	
 	private function verify_input()
 	{
-		//TODO...
+		//TODO: password stuff...anything else?
+		if(strlen($this->f_cell_phone_number) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: Cell Phone Number is too long.';
+		if(strlen($this->f_dept_name) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: Department Name is too long.';
+		if(strlen($this->f_email) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: Email is too long.';
+		if(strlen($this->f_fax_number) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: Fax Number is too long.';
+		if(strlen($this->f_first_name) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: First Name is too long.';
+		if(strlen($this->f_icode) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: ID Code is too long.';
+		if(strlen($this->f_last_name) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: Last Name is too long.';
+		if(strlen($this->f_office_location) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: Office Location is too long.';
+		if(strlen($this->f_office_phone_number) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: Office Phone Number is too long.';
+		if(strlen($this->f_password) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: Password is too long.';
+		if(strlen($this->f_title) > Config::$DEFAULT_VARCHAR_LEN)
+			$this->m_err_msg[sizeof($this->m_err_msg)] = 'Error: Title is too long.';
 		
 		/*
 		//Error Handling Example:
@@ -165,7 +203,8 @@ class PageEmployeeAddEdit
 				$this->f_icode . ' ' .
 				$this->f_last_name . ' ' .
 				$this->f_office_location . ' ' .
-				$this->f_office_phone_number				
+				$this->f_office_phone_number . ' ' .
+				$this->f_title			
 			);
 			
 			//edit mode submit
@@ -190,6 +229,7 @@ class PageEmployeeAddEdit
 						office_location=\'' . $this->f_office_location . '\',
 						office_phone_number=\'' . $this->f_office_phone_number . '\',
 						password=\'' . $this->f_password . '\',
+						title=\'' . $this->f_title . '\',
 						updated_employee_id=\'' . LoginManager::get_id() . '\',
 						search_words=\'' . $search_words . '\',
 						updated_date = NOW()
@@ -247,6 +287,7 @@ class PageEmployeeAddEdit
 						office_location,
 						office_phone_number,
 						password,
+						title,
 						created_employee_id,
 						updated_employee_id,
 						created_date,
@@ -266,6 +307,7 @@ class PageEmployeeAddEdit
 						\'' . $this->f_office_location . '\',
 						\'' . $this->f_office_phone_number . '\',
 						\'' . $this->f_password . '\',
+						\'' . $this->f_title . '\',
 						\'' . LoginManager::get_id() . '\',
 						\'' . LoginManager::get_id() . '\',
 						NOW(),
@@ -282,44 +324,8 @@ class PageEmployeeAddEdit
 		}
 	}
 	
-	private function show_output($err_msg)
+	private function show_admin_output()
 	{
-		//echo the outer area with the correct tab highlighted for this page
-		ObjOuterArea::echo_top(ObjOuterArea::$TAB_EMPLOYEES);
-		
-		//echo inner area html here
-		echo
-		('
-               <div align="center">
-                  <form name="form1" method="post" action="page_employee_add_edit.php?f_action=submit&f_mode=' . IO::prepout_url($this->f_mode) . (($this->f_mode == 'edit') ? ('&f_id=' . IO::prepout_url($this->f_id)) : '') . '">
-                    <table width="600" border="0" cellpadding="0" cellspacing="0">
-                      <tr>
-                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                            <tr>
-                              <td width="25%" align="right" valign="middle">&nbsp;</td>
-                              <td width="75%" align="left" valign="middle" class="text_title">Add New Employee</td>
-                            </tr>
-                        </table></td>
-                      </tr>	
-		');	
-		
-		if (sizeof($err_msg) > 0) 
-		{
-			foreach ($err_msg as $entity)
-			{
-				echo('
-							  <tr>
-								<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
-									<tr>
-									  <td width="25%" align="right" valign="middle">&nbsp;</td>
-									  <td width="75%" align="left" valign="middle" class="text_error">' . $entity . '</td>
-									</tr>
-								</table></td>
-							  </tr>
-				');
-			}
-		}
-		
 		echo('			  
                       <tr>
                         <td>&nbsp;</td>
@@ -337,6 +343,10 @@ class PageEmployeeAddEdit
                             <tr>
                               <td width="25%" align="right" valign="middle" class="text_label">Last Name:&nbsp;</td>
                               <td width="75%" align="left" valign="middle"><input name="f_last_name" type="text" size="32" class="textbox" value="' . IO::prepout_sl($this->f_last_name, false) . '"></td>
+                            </tr>
+                            <tr>
+                              <td width="25%" align="right" valign="middle" class="text_label">Title:&nbsp;</td>
+                              <td width="75%" align="left" valign="middle"><input name="f_title" type="text" size="32" class="textbox" value="' . IO::prepout_sl($this->f_title, false) . '"></td>
                             </tr>
                             <tr>
                               <td width="25%" align="right" valign="middle" class="text_label">Email:&nbsp;</td>
@@ -404,13 +414,168 @@ class PageEmployeeAddEdit
                   </form>
                 </div>		
 		');
+	}
+	
+	private function show_user_output()
+	{
+		echo('			  
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>						
+                      <tr>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                              <td width="25%" align="right" valign="middle" class="text_label">First Name:&nbsp;</td>
+                              <td width="75%" align="left" valign="middle"><input name="f_first_name" type="text" size="32" class="textbox" value="' . IO::prepout_sl($this->f_first_name, false) . '"></td>
+                            </tr>
+                            <tr>
+                              <td width="25%" align="right" valign="middle" class="text_label">Last Name:&nbsp;</td>
+                              <td width="75%" align="left" valign="middle"><input name="f_last_name" type="text" size="32" class="textbox" value="' . IO::prepout_sl($this->f_last_name, false) . '"></td>
+                            </tr>
+                            <tr>
+                              <td width="25%" align="right" valign="middle" class="text_label">Email:&nbsp;</td>
+                              <td width="75%" align="left" valign="middle"><input name="f_email" type="text" size="24" class="textbox" value="' . IO::prepout_sl($this->f_email, false) . '"></td>
+                            </tr>
+                            <tr>
+                              <td width="25%" align="right" valign="middle" class="text_label">Office Phone Number:&nbsp;</td>
+                              <td width="75%" align="left" valign="middle"><input name="f_office_phone_number" type="text" size="24" class="textbox" value="' . IO::prepout_sl($this->f_office_phone_number, false) . '"></td>
+                            </tr>
+                            <tr>
+                              <td width="25%" align="right" valign="middle" class="text_label">Cell Phone Number:&nbsp;</td>
+                              <td width="75%" align="left" valign="middle"><input name="f_cell_phone_number" type="text" size="24" class="textbox" value="' . IO::prepout_sl($this->f_cell_phone_number, false) . '"></td>
+                            </tr>
+                            <tr>
+                              <td width="25%" align="right" valign="middle" class="text_label">Fax Number:&nbsp;</td>
+                              <td width="75%" align="left" valign="middle"><input name="f_fax_number" type="text" size="24" class="textbox" value="' . IO::prepout_sl($this->f_fax_number, false) . '"></td>
+                            </tr>
+                        </table></td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>	
+                      <tr>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tr>
+                            <td width="25%" align="right" valign="middle" class="text_label">Office Location:&nbsp;</td>
+                            <td width="75%" align="left" valign="middle"><input name="f_office_location" type="text" size="40" class="textbox" value="' . IO::prepout_sl($this->f_office_location, false) . '"></td>
+                          </tr>
+                          <tr>
+                            <td width="25%" align="right" valign="middle" class="text_label">Password:&nbsp;</td>
+                            <td width="75%" align="left" valign="middle"><input name="f_password" type="text" size="24" class="textbox" value="' . IO::prepout_sl($this->f_password, false) . '"></td>
+                          </tr>
+                        </table></td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                              <td width="25%" align="right" valign="top">&nbsp;</td>
+                              <td width="75%" align="left" valign="middle"><input type="submit" name="Submit" value="Save" class="button"></td>
+                            </tr>
+                        </table></td>
+                      </tr>
+                    </table>
+                  </form>
+                </div>		
+		');
+	}
+	
+	private function show_output($err_msg)
+	{
+		//echo the outer area with the correct tab highlighted for this page
+		ObjOuterArea::echo_top(ObjOuterArea::$TAB_EMPLOYEES);
 		
+		//echo inner area html here
+		if($this->f_mode == 'edit')
+		{
+			if($this->isAdmin())
+			{
+				echo
+				('
+	               <div align="center">
+	                  <form name="form1" method="post" action="page_employee_add_edit.php?f_action=submit&f_mode=' . IO::prepout_url($this->f_mode) . (($this->f_mode == 'edit') ? ('&f_id=' . IO::prepout_url($this->f_id)) : '') . '">
+	                    <table width="600" border="0" cellpadding="0" cellspacing="0">
+	                      <tr>
+	                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+	                            <tr>
+	                              <td width="25%" align="right" valign="middle">&nbsp;</td>
+	                              <td width="75%" align="left" valign="middle" class="text_title">Edit Employee</td>
+	                            </tr>
+	                        </table></td>
+	                      </tr>	
+				');
+			}
+			else
+			{
+				echo
+				('
+	               <div align="center">
+	                  <form name="form1" method="post" action="page_employee_add_edit.php?f_action=submit&f_mode=' . IO::prepout_url($this->f_mode) . (($this->f_mode == 'edit') ? ('&f_id=' . IO::prepout_url($this->f_id)) : '') . '">
+	                    <table width="600" border="0" cellpadding="0" cellspacing="0">
+	                      <tr>
+	                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+	                            <tr>
+	                              <td width="25%" align="right" valign="middle">&nbsp;</td>
+	                              <td width="75%" align="left" valign="middle" class="text_title">Edit Profile</td>
+	                            </tr>
+	                        </table></td>
+	                      </tr>	
+				');
+			}
+			
+		}
+		else
+		{
+			echo
+			('
+	               <div align="center">
+	                  <form name="form1" method="post" action="page_employee_add_edit.php?f_action=submit&f_mode=' . IO::prepout_url($this->f_mode) . (($this->f_mode == 'edit') ? ('&f_id=' . IO::prepout_url($this->f_id)) : '') . '">
+	                    <table width="600" border="0" cellpadding="0" cellspacing="0">
+	                      <tr>
+	                        <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+	                            <tr>
+	                              <td width="25%" align="right" valign="middle">&nbsp;</td>
+	                              <td width="75%" align="left" valign="middle" class="text_title">Add New Employee</td>
+	                            </tr>
+	                        </table></td>
+	                      </tr>	
+			');
+		}
+			
+		
+		if (sizeof($err_msg) > 0) 
+		{
+			foreach ($err_msg as $entity)
+			{
+				echo('
+							  <tr>
+								<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+									<tr>
+									  <td width="25%" align="right" valign="middle">&nbsp;</td>
+									  <td width="75%" align="left" valign="middle" class="text_error">' . $entity . '</td>
+									</tr>
+								</table></td>
+							  </tr>
+				');
+			}
+		}
+		
+		if($this->isAdmin())
+			$this->show_admin_output();
+		else
+			$this->show_user_output();	
 		
 		ObjOuterArea::echo_bottom();
 	
 		//output is always the last thing done when called
 		exit();
 	}
+
 }
 
 //create an instance of the page and run it
