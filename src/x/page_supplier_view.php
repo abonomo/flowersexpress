17a22f58e3019aca1ca4bc1b7876f417c69ffcb5
abonomo <abonomo@88aed6c4-299c-11de-a747-197756c87c9e>
2009-05-19 19:49:10 +0000

    -added a "Just Called" button to customer and supplier view pages
    -modified database to add last_contacted field to customers and suppliers

--- src/x/page_supplier_view.php
+++ src/x/page_supplier_view.php
@@ -8,6 +8,7 @@ class PageSupplierView
 	private static $THIS_PAGE = 'page_supplier_view.php';
 	
 	//*** MEMBERS ***
+	
 	private $f_mode;	
 	private $f_action;
 	
@@ -26,8 +27,8 @@ class PageSupplierView
 	private $f_province;
 	private $f_country;
 	private $f_notes;
-	private $f_trash_flag;	
 	
+				//ADD THIS FOR UPDATED INFO FUNCTIONALITY
 	private $f_created_first;
 	private $f_created_last;
 	private $f_updated_first;
@@ -35,6 +36,8 @@ class PageSupplierView
 	
 	private $f_created_date;
 	private $f_updated_date;
+	private $f_last_contacted;
+			//to this
 	
 	//*** FUNCTIONS ***
 	//execution entry point
@@ -57,6 +60,18 @@ class PageSupplierView
 	private function get_input()
 	{
 		$this->f_id = IO::get_input_sl_g('f_id', 'string');
+		$this->f_action = IO::get_input_sl_pg('f_action', 'string');
+		if($this->f_action == 'just_called')
+		{
+			DB::send_query
+			('
+				UPDATE suppliers SET
+				last_contacted = NOW(),
+				updated_date = NOW(),
+				updated_employee_id=\'' . LoginManager::get_id() . '\'
+				WHERE id = \'' . $this->f_id . '\'
+			');
+		}
 	}
 	
 	private function verify_input()
@@ -70,7 +85,7 @@ class PageSupplierView
 	
 	private function get_output()
 	{
-			//Query for supplier info and created employee info from database
+					//get values from database
 			$suppl_info = DB::get_single_row_fq
 			('
 				SELECT  suppliers.icode, 
@@ -86,12 +101,12 @@ class PageSupplierView
 						suppliers.province, 
 						suppliers.country, 
 						suppliers.notes,
-						suppliers.trash_flag,
 						
 						suppliers.created_employee_id, 
 						suppliers.updated_employee_id,
 						suppliers.created_date,
 						suppliers.updated_date,
+						suppliers.last_contacted,
 						
 						employees.first_name,
 						employees.last_name
@@ -99,30 +114,6 @@ class PageSupplierView
 				LEFT OUTER JOIN employees ON suppliers.created_employee_id = employees.id
 				WHERE suppliers.id=\'' . $this->f_id . '\''
 			);
-			
-			$this->f_icode 					= $suppl_info['icode'];
-			$this->f_company_name			= $suppl_info['company_name'];
-			$this->f_contact_name 			= $suppl_info['contact_name'];
-			$this->f_contact_dept 			= $suppl_info['contact_dept'];
-			$this->f_office_phone_number 	= $suppl_info['office_phone_number'];
-			$this->f_cell_phone_number 		= $suppl_info['cell_phone_number'];
-			$this->f_fax_number 			= $suppl_info['fax_number'];
-			$this->f_address_line_1 		= $suppl_info['address_line_1'];
-			$this->f_address_line_2 		= $suppl_info['address_line_2'];
-			$this->f_city 					= $suppl_info['city'];
-			$this->f_province 				= $suppl_info['province'];
-			$this->f_country 				= $suppl_info['country'];
-			$this->f_notes 					= $suppl_info['notes'];	
-			$this->f_created_employee_id	= $suppl_info['created_employee_id'];
-			$this->f_updated_employee_id	= $suppl_info['updated_employee_id'];
-			
-			$this->f_created_first 			= $suppl_info['first_name'];
-			$this->f_created_last 			= $suppl_info['last_name'];
-			
-			$this->f_created_date 			= $suppl_info['created_date'];
-			$this->f_updated_date 			= $suppl_info['updated_date'];
-			
-			//QUERY for updated employee info
 			$suppl_info_up = DB::get_single_row_fq
 			('
 				SELECT  employees.first_name, 
@@ -131,9 +122,31 @@ class PageSupplierView
 				LEFT OUTER JOIN employees ON suppliers.updated_employee_id = employees.id
 				WHERE suppliers.id=\'' . $this->f_id . '\''
 			);
-		
-			$this->f_updated_first 			= $suppl_info_up['first_name'];
-			$this->f_updated_last 			= $suppl_info_up['last_name'];
+			
+			$this->f_icode = $suppl_info['icode'];
+			$this->f_company_name = $suppl_info['company_name'];
+			$this->f_contact_name = $suppl_info['contact_name'];
+			$this->f_contact_dept = $suppl_info['contact_dept'];
+			$this->f_office_phone_number = $suppl_info['office_phone_number'];
+			$this->f_cell_phone_number = $suppl_info['cell_phone_number'];
+			$this->f_fax_number = $suppl_info['fax_number'];
+			$this->f_address_line_1 = $suppl_info['address_line_1'];
+			$this->f_address_line_2 = $suppl_info['address_line_2'];
+			$this->f_city = $suppl_info['city'];
+			$this->f_province = $suppl_info['province'];
+			$this->f_country = $suppl_info['country'];
+			$this->f_notes = $suppl_info['notes'];	
+			$this->f_last_contacted = $suppl_info['last_contacted'];
+			
+					//add these for UPDATED INFO
+			$this->f_created_first = $suppl_info['first_name'];
+			$this->f_created_last = $suppl_info['last_name'];
+			
+			$this->f_updated_first = $suppl_info_up['first_name'];
+			$this->f_updated_last = $suppl_info_up['last_name'];
+			
+			$this->f_created_date = $suppl_info['created_date'];
+			$this->f_updated_date = $suppl_info['updated_date'];
 			
 	}
 	
@@ -144,8 +157,8 @@ class PageSupplierView
 		
 		//echo inner area html
 		Echo ('	
-			<div align="center">
-			  <form name="form1" method="post" action="page_supplier_view.php">
+				   <div align="center">
+			  <form name="form1" method="post" action="page_supplier_view.php?f_id=' . IO::prepout_url($this->f_id) . '&f_action=just_called">
 				<table width="600" border="0" cellpadding="0" cellspacing="0">
 
 				  <tr>
@@ -158,128 +171,117 @@ class PageSupplierView
 				  </tr>	
 				  
 				  <tr>
+
 					<td>&nbsp;</td>
-				  </tr>	
-				  
+				  </tr>						
 				  <tr>
 					<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
-					
 						<tr>
 						  <td width="25%" align="right" valign="middle" class="text_label">ID Code:&nbsp;</td>
-						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_icode) . '');
-						  
-						 //if the shipper has been deleted, print trash icon
-						if( $this->f_trash_flag == '1' )
-						{
-							echo ('<img src="../img/trash.gif"/> ');
-						}
-						
-						echo('</td>
+						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_icode) . '</td>
 						</tr>
-						
 						<tr>
+
 						  <td width="25%" align="right" valign="middle" class="text_label">Company Name:&nbsp;</td>
 						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_company_name) . '</td>
 						</tr>
-						
 						<tr>
 						  <td width="25%" align="right" valign="middle" class="text_label">Contact Name:&nbsp;</td>
 						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_contact_name) . '</td>
 						</tr>
-						
 						<tr>
+
 						  <td width="25%" align="right" valign="middle" class="text_label">Contact Department:&nbsp;</td>
 						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_contact_dept) . '</td>
 						</tr>
-						
 						<tr>
 						  <td width="25%" align="right" valign="middle" class="text_label">Office Phone:&nbsp;</td>
 						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_office_phone_number) . '</td>
 						</tr>
-						
 						<tr>
 						  <td width="25%" align="right" valign="middle" class="text_label">Cell Phone:&nbsp;</td>
 						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_cell_phone_number) . '</td>
 						</tr>
-						
 						<tr>
 						  <td width="25%" align="right" valign="middle" class="text_label">Fax:&nbsp;</td>
 						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_fax_number) . '</td>
 						</tr>
-						
 					</table></td>
+
 				  </tr>
-				  
 				  <tr>
 					<td>&nbsp;</td>
 				  </tr>	
-				  
 				  <tr>
 					<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
-					
 					  <tr>
 						<td width="25%" align="right" valign="middle" class="text_label">Address Line 1:&nbsp;</td>
 						<td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_address_line_1) . '</td>
+
 					  </tr>
-					  
 					  <tr>
 						<td width="25%" align="right" valign="middle" class="text_label">Address Line 2:&nbsp;</td>
 						<td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_address_line_2) . '</td>
 					  </tr>
-					  
 					  <tr>
 						<td width="25%" align="right" valign="middle" class="text_label">City:&nbsp;</td>
 						<td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_city) . '</td>
+
 					  </tr>
-					  
 					  <tr>
 						<td width="25%" align="right" valign="middle" class="text_label">Province:&nbsp;</td>
 						<td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_province) . '</td>
 					  </tr>
-					  
 					  <tr>
 						<td width="25%" align="right" valign="middle" class="text_label">Country:&nbsp;</td>
 						<td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_country) . '</td>
+
 					  </tr>
-					  
 					</table></td>
 				  </tr>
-				  
-				  <tr>
+			  <tr>
 					<td>&nbsp;</td>
 				  </tr>	
-				  
 				  <tr>
-				    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
-				  
+				<!--BEGINNNN updated info area-->
+				  <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
 						<tr>
 						  <td width="25%" align="right" valign="middle" class="text_label">Created:&nbsp;</td>
-						  <td width="75%" align="left" valign="middle"><a href="page_employee_view.php?f_id=' . IO::prepout_sl($this->f_created_employee_id, false) . '">'  . 
-									IO::prepout_sl($this->f_created_first, false) . '&nbsp;' . IO::prepout_sl($this->f_created_last, false)  . '</a>&nbsp;on&nbsp;' . 
-									IO::prepout_ml_html($this->f_created_date) . '</td>
+						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_created_first) . '&nbsp;' . IO::prepout_ml_html($this->f_created_last) . '&nbsp;on&nbsp;' . IO::prepout_ml_html($this->f_created_date) . '</td>
 						 </tr>
-						 
 				  		<tr>
 						  <td width="25%" align="right" valign="middle" class="text_label">Updated:&nbsp;</td>
-						  <td width="75%" align="left" valign="middle"><a href="page_employee_view.php?f_id=' . IO::prepout_sl($this->f_updated_employee_id, false) . '">'  . 
-									IO::prepout_sl($this->f_updated_first, false) . '&nbsp;' . IO::prepout_sl($this->f_updated_last, false)  . '</a>&nbsp;on&nbsp;' . 
-									IO::prepout_ml_html($this->f_updated_date) . '</td>
+						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_updated_first) . '&nbsp;' . IO::prepout_ml_html($this->f_updated_last) . '&nbsp;on&nbsp;' . IO::prepout_ml_html($this->f_updated_date) . '</td>
 						 </tr>
-						 
+						<tr>
+						   <td width="25%" align="right" valign="middle" class="text_label">Last Contacted:&nbsp;</td>
+						   <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_last_contacted) . '</td>
+					  	</tr>
 					</table></td>
+					<!--ENDDDDDDD updated/created info area-->
 				  </tr>
-				  
 				  <tr>
 					<td>&nbsp;</td>
 				  </tr>	
-				 
 				  <tr>
 					<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
 						<tr>
-
 						  <td width="25%" align="right" valign="top" class="text_label">Notes:&nbsp;</td>
 						  <td width="75%" align="left" valign="middle">' . IO::prepout_ml_html($this->f_notes) . '</td>
 						</tr>
+					</table></td>
+				  </tr>
+				  <tr>
+					<td>&nbsp;</td>
+				  </tr>
+					<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    	<tr>
+                        	<td width="25%" align="right" valign="top">&nbsp;</td>
+                            <td width="75%" align="left" valign="middle"><input type="submit" name="Just Called" value="Just Called" class="button"></td>
+                        </tr>
+                    </table></td>
+				  <tr>
+					<td><table width="100%" border="0" cellspacing="0" cellpadding="0">
 					</table></td>
 				  </tr>
 				</table>
