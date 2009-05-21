7cf44cdbcab5f64519423842d4637418d8a956a2
paulse <paulse@88aed6c4-299c-11de-a747-197756c87c9e>
2009-05-21 19:22:15 +0000

    forgot to upload these last night. work correctly afaict.
    moving on.

---
+++ trunk/src/x/page_purchase_report.php
@@ -0,0 +1,156 @@
+<?php
+
+require_once('framework.php');
+require_once('our_time.php');
+
+class PageTemplate
+{
+	//*** CONSTANTS ***
+	private static $THIS_PAGE = 'page_template.php';
+	
+	//*** MEMBERS ***
+	private $m_obj_info_arr;
+	
+	//*** FUNCTIONS ***
+	//execution entry point
+	
+	public function run()
+	{
+		session_start();
+		DB::connect();
+	
+		LoginManager::assert_auth_level(LoginManager::$AUTH_READ_ONLY);	//CHANGE required authorization level for this page, ADMIN is the strictest
+	
+		$this->get_input(); 
+		
+		$this->verify_input();
+		
+		$this->process_input();
+		
+		$this->show_output();
+	}
+	
+	private function get_input()
+	{
+		$this->f_report_start = IO::get_input_sl_pg('f_report_start','string');
+		$this->f_report_end = IO::get_input_sl_pg('f_report_end','string');		
+	}
+	
+	private function verify_input()
+	{
+		/*
+		//Error Handling Example:
+		if(something is bad) $this->show_output('Error: Field X needs to be corrected');
+		*/
+	}
+	
+	
+	private function process_input()
+	{
+		$report_start_datetime = OurTime::js_to_datetime($this->f_report_start, 0);
+		$report_end_datetime = OurTime::js_to_datetime($this->f_report_end, 1);
+		
+		$this->m_obj_info_arr = DB::get_all_rows_fq ('
+			SELECT purchases.*,
+			suppliers.icode AS supplier_icode,
+			suppliers.company_name AS supplier_company_name,
+			shippers.icode AS shipper_icode,
+			shippers.company_name AS shipper_company_name
+			FROM purchases
+			LEFT OUTER JOIN suppliers ON purchases.supplier_id = suppliers.id
+			LEFT OUTER JOIN shippers ON purchases.shipper_id = shippers.id
+			WHERE purchases.delivery_date > \'' . $report_start_datetime . '\' AND purchases.delivery_date < \'' . $report_end_datetime . '\'
+
+		');
+		
+		//TESTING: show how many rows we got:
+		//echo count($this->m_obj_info_arr);*/
+
+	}
+	
+	private function show_output($err_msg = '')
+	{
+		//echo inner area html here
+		/*
+		//Error Printing Example:
+		if($err_msg != '') echo('<font class="text_error">' . $err_msg . '</font>');
+		*/
+					
+		echo ('
+			<html>
+			<head>
+				<link href="style_report.css" rel="stylesheet">
+			</head>
+
+			<body>
+			
+			<table>
+				<tr>
+					<td>
+						<h2>Purchase Order Report</h2>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<h4>Dates: ' . IO::prepout_sl($this->f_report_start, false) . ' to '. IO::prepout_sl($this->f_report_end, false) . '</h4>
+					</td>
+				</tr>
+			</table>
+		');
+		
+		echo ('<table border="1" width="98%" cellspacing="0" cellpadding="0" class="report_table"> 
+			<tr> 
+				<th>&nbsp;Order ID&nbsp;</th> 
+				<th>&nbsp;Notes&nbsp;</th> 
+				<th>&nbsp;Supplier ID&nbsp;</th> 
+				<th>&nbsp;Supplier&nbsp;</th> 
+				<th>&nbsp;Shipper ID&nbsp;</th> 
+				<th>&nbsp;Shipper&nbsp;</th> 
+				<th>&nbsp;Shipment Details&nbsp;</th> 
+				<th>&nbsp;In Warehouse&nbsp;</th> 
+				<th>&nbsp;Delivery Date&nbsp;</th> 
+				<th>&nbsp;Created Date&nbsp;</th> 
+				<th>&nbsp;Updated Date&nbsp;</th> 
+			</tr> '); 
+
+		for ($i = 0; $i < count($this->m_obj_info_arr); $i++) 
+		{
+			$major_array = $this->m_obj_info_arr[$i];		
+		
+			echo ('
+			<tr><td>&nbsp;'. IO::prepout_sl($major_array['icode'], false) . '&nbsp;</td>
+				<td>&nbsp;'. IO::prepout_ml_html($major_array['notes'], false) . '&nbsp;</td>
+				<td>&nbsp;'. IO::prepout_sl($major_array['supplier_icode'], false) . '&nbsp;</td>
+				<td>&nbsp;'. IO::prepout_sl($major_array['supplier_company_name'], false) . '&nbsp;</td>
+				<td>&nbsp;'. IO::prepout_sl($major_array['shipper_icode'], false) . '&nbsp;</td>
+				<td>&nbsp;'. IO::prepout_sl($major_array['shipper_company_name'], false) . '&nbsp;</td>
+				<td>&nbsp;'. IO::prepout_ml_html($major_array['shipment_details'], false) . '&nbsp;</td>
+				');
+				if($major_array['in_warehouse'] ==1)
+					echo('<td>&nbsp;Yes&nbsp;</td>');
+				else
+					echo('<td>&nbsp;No&nbsp;</td>');
+			echo ('
+				<td>&nbsp;'. IO::prepout_sl($major_array['delivery_date'], false) . '&nbsp;</td>
+				<td>&nbsp;'. IO::prepout_sl($major_array['created_date'], false) . '&nbsp;</td>
+				<td>&nbsp;'. IO::prepout_sl($major_array['updated_date'], false) . '&nbsp;</td>
+			</tr>
+			');  
+		}
+		
+		echo('
+		</table>
+		</body>
+		</html>
+		');
+			
+		//output is always the last thing done when called
+		exit();
+	}
+}
+
+//create an instance of the page and run it
+$page_template = new PageTemplate();
+$page_template->run();
+
+?>
--- trunk/src/x/page_report_purchase_menu.php
+++ trunk/src/x/page_report_purchase_menu.php
@@ -2,7 +2,7 @@
 
 require_once('framework.php');
 
-class PagePurchaseMenu
+class PageReportPurchaseMenu
 {
 	//*** CONSTANTS ***
 	private static $THIS_PAGE = 'page_report_purchase_menu.php';
@@ -55,18 +55,22 @@ class PagePurchaseMenu
 		//echo inner area html here
 		//  TODO : Search algorithm
 			echo ('
-			<form name="form">
+			<form name="form_report_menu" method="post" action="page_purchase_report.php">
 				<div class="text_title">Purchase Report Menu</div>
-				<input name="f_report_search" class="textbox" type="text" /><input value="Search" type="button" onclick="document.location=(\'page_report_list.php?f_search=\' + form.f_report_search.value)" /><br>
-				<a href="page_report_purchase_order_daily.php">Daily Purchase Order</a><br>
-				<a href="page_report_purchaes_order_monthly.php">Monthly Purchase Order</a><br>
-				<a href="page_report_purchase_order_range.php">Purchase Order : Range</a><br>
-				<a href="page_report_supplier.php">Supplier Information</a><br>
+				<input name="f_report_search" class="textbox" type="text" /><input value="Search" type="button" onclick="document.location=(\'page_report_list.php?f_search=\' + form.f_report_search.value)" /><br><br>
+				
+				Select Range of Delivery dates:<br>
+				<input name="f_report_start" class="textbox" type="text" /> <a href="#" onclick="calbeg.select(document.forms[0].f_report_start, \'datexx\', \'MM/dd/yyyy\'); return false; " name="datexx" id="datexx"> Select Begin Date</a>
+				<div id="testdiv1" style="position:absolute;visibility:hidden;"></div><br>
+				
+				<input name="f_report_end" class="textbox" type="text" /> <a href="#" onclick="calend.select(document.forms[0].f_report_end, \'datex\', \'MM/dd/yyyy\'); return false; " name="datex" id="datex"> Select End Date</a>
+				<div id="testdiv2" style="position:absolute;visibility:hidden;"></div><br><br>
+				
+				<a href="#" onclick="form_report_menu.submit(); return false;">Get Purchase Orders</a><br>
+				
 			</form>
 			');
 			
-			
-
 
 
 		
@@ -78,7 +82,7 @@ class PagePurchaseMenu
 }
 
 //create an instance of the page and run it
-$page_template = new PagePurchaseMenu();
-$page_template->run();
+$page_purchase_report_menu = new PageReportPurchaseMenu();
+$page_purchase_report_menu->run();
 
 ?>
