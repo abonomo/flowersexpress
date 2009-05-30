dbf94ecdcddd5680178acfff59002e2be0606c03
abonomo <abonomo@88aed6c4-299c-11de-a747-197756c87c9e>
2009-05-30 22:00:04 +0000

    added delete/undelete functionality to employees

---
+++ trunk/src/x/page_employee_undelete.php
@@ -0,0 +1,109 @@
+<?php
+
+require_once('framework.php');
+
+class PageEmployeeUndelete
+{
+	//*** CONSTANTS ***
+	private static $THIS_PAGE = 'page_employee_undelete.php';
+	
+	//*** MEMBERS ***
+	private $f_id;
+	
+	//*** FUNCTIONS ***
+	//execution entry point
+	public function run()
+	{
+		session_start();
+		DB::connect();
+	
+		LoginManager::assert_auth_level(LoginManager::$AUTH_ADMIN);	
+	
+		$this->get_input(); 
+		
+		$this->verify_input();
+		
+		$this->process_input();
+		
+		$this->show_output();
+	}
+	
+	private function get_input()
+	{
+		//get id of employee to undelete
+		$this->f_id = IO::get_input_sl_g('f_id','string');
+	}
+	
+	private function verify_input()
+	{
+		if($this->f_id == "") 
+			$this->show_output('Employee not found.');
+	}
+	
+	private function process_input()
+	{
+		if( DB::bool_get_single_row_fq('SELECT * FROM employees where id =\'' . $this->f_id .'\'') )
+		{
+			//update trash_flag
+			DB::send_query('
+			UPDATE employees SET
+			trash_flag = 0
+			WHERE id=\'' . $this->f_id . '\'
+			');
+		}
+		else
+		{
+			// was not a unique result
+			$this->show_output('Employee not found');
+		}
+	}
+	
+	private function show_output($err_msg = '')
+	{
+		//echo the outer area with the correct tab highlighted for this page
+		ObjOuterArea::echo_top(ObjOuterArea::$TAB_EMPLOYEES);
+		
+		//echo inner area html here
+		echo ('
+		<!-- Title of the page -->
+		<table width="100%">
+			<tr>
+				<td style="text-align:center" class="text_title">
+				Undelete Employee
+				</td>
+			</tr> ');
+
+		if($err_msg != '')
+		{
+			echo ('
+				<!-- Error message -->
+				<tr>
+					<td>' . $err_msg . '</td>
+				</tr>
+			');
+		}
+		else
+		{
+			echo ('
+				<tr>
+					<td align="center"> Employee has been undeleted.</td>
+				</tr>
+			');
+		}
+		
+		echo ('
+			</table>
+		');
+		
+		ObjOuterArea::echo_bottom();
+	
+		//output is always the last thing done when called
+		exit();
+	}
+}
+
+//create an instance of the page and run it
+$page_employee_undelete = new PageEmployeeUndelete();
+$page_employee_undelete->run();
+
+?>
